417771d56f5de0a2aad1aba731b15bfb556017d7
Kevin Leblanc <kevin.leblanc.1@etsmtl.net>
2016-01-30 00:44:29 -0500

    Transparent Walls

---
+++ TheGrandAwakening/The Grand Awakening/Assets/Scripts/Walls.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walls : MonoBehaviour {
+
+	public GameObject player;
+	Color color;
+
+
+	// Use this for initialization
+	void Start () {
+		color = GetComponent<Renderer> ().material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Renderer> ().material.color = color;
+		if (transform.position.z < player.transform.position.z) {
+			color.a = 0.2f;
+		}
+		else {
+			color.a = 1f;
+		}
+	}
+}
