cea6a2e4eff621576883e803ef00195ca5b37839
max89max <maxime.larivierem@gmail.com>
2016-01-30 16:35:41 +0100

    Sprite renderer

--- TheGrandAwakening/The Grand Awakening/Assets/Scripts/Walls.cs
+++ TheGrandAwakening/The Grand Awakening/Assets/Scripts/Walls.cs
@@ -10,11 +10,11 @@ public class Walls : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		color = GetComponent<Renderer> ().material.color;
-		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.Log (transform.position.z + " ## " + player.transform.position.z);
 		GetComponent<Renderer> ().material.color = color;
 		if (transform.position.z < player.transform.position.z) {
 			color.a = 0.2f;
