80fc6a881fc79dd6b764c7f208bf16e5a00773c1
Kevin Leblanc <kevin.leblanc.1@etsmtl.net>
2016-01-29 23:19:16 -0500

    Player Movement

---
+++ TheGrandAwakening/The Grand Awakening/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+	float playerSpeed = 0.1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetButton ("Left")) {
+			transform.Translate (new Vector3 (-playerSpeed, 0f, 0f));
+		}
+		if (Input.GetButton ("Right")) {
+			transform.Translate (new Vector3 (playerSpeed, 0f, 0f));
+		}
+		if (Input.GetButton ("Up")) {
+			transform.Translate (new Vector3 (0f, 0f, playerSpeed));
+		}
+		if (Input.GetButton ("Down")) {
+			transform.Translate (new Vector3 (0f, 0f, -playerSpeed));
+		}
+	}
+}
