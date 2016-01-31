af5ef00c7339c03539ee0bde26098dfc11d4213b
Joel <joel.lapointe.86@gmail.com>
2016-01-30 22:48:12 -0500

    game over 2/3 - todo: calculate and display high score

--- TheGrandAwakening/The Grand Awakening/Assets/Scripts/Timer.cs
+++ TheGrandAwakening/The Grand Awakening/Assets/Scripts/Timer.cs
@@ -5,27 +5,37 @@ public class Timer : MonoBehaviour {
 
 	private float timer = 0.0f;
 	public float timeLeft = 180.0f; //start at 180s
-	bool timeUPs = false;
+	public bool timeUPs = false;
 
 	private float minutes;
 	private float seconds;
 
 	public Font guiFont;
 	private GUIStyle guiStyle = new GUIStyle();
+	private string text;
+
+	private typerAuto typer;
+	private Animator gameOverAnim;
 
 
 	void Start(){
 		timer = timeLeft;
 	}
 
+	void Awake(){
+		typer = GetComponentInChildren<typerAuto> ();
+		gameOverAnim = GetComponent<Animator> ();
+	}
+
 	// Update is called once per frame
 	void Update () {
-
 		timer -= Time.deltaTime;
 
-		if (timer <= 0) {
+		if ( (timer <= 0) && (!timeUPs) ) {
 			timeUPs = true;
-			//TODO
+
+			gameOverAnim.SetBool ("fadeIn",true);
+			typer.StartCoroutine ("TypeIn"); 
 
 			//calculate score
 			//end game
@@ -43,7 +53,10 @@ public class Timer : MonoBehaviour {
 		//guiStyle.normal.textColor = Color.red;
 		minutes = Mathf.FloorToInt(timer / 60.0f);
 		seconds = Mathf.FloorToInt(timer % 60.0f);
-		string text = string.Format("{0:00}:{1:00}", minutes.ToString("0"), seconds.ToString("00"));
+		if(timer >= 0)
+			text = string.Format("{0:00}:{1:00}", minutes.ToString("0"), seconds.ToString("00"));
+		else
+			text = "0:00";
 		GUI.Label(new Rect(10, 5, 100, 20), "Time: " + text, guiStyle);
 
 	}
