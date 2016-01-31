4d72f52c6bf695e2f47176ec6a7040c3f824d92b
Joel <joel.lapointe.86@gmail.com>
2016-01-31 14:13:33 -0500

    added different msg to end game

--- TheGrandAwakening/The Grand Awakening/Assets/Scripts/typerAuto.cs
+++ TheGrandAwakening/The Grand Awakening/Assets/Scripts/typerAuto.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class typerAuto : MonoBehaviour {
 
 
-	private string gameOverMsg = "GameOver! Time's up! \n \n Score : ";
+	private string gameOverMsg = "GameOver! Time's up! \n \n You only scored : ";
+	private string winMsg = "GameOver! Time's up! \n \n Nice! You scored : ";
+	private string msgToDisplay = "placeholder";
 	private Text textComp;
 	public float startDelay = 2.0f;
 	public float typeDelay = 0.05f;
@@ -23,23 +25,21 @@ public class typerAuto : MonoBehaviour {
 	//auto type text
 	public IEnumerator TypeIn(){
 		score = points.getPoints ();
-		gameOverMsg += score.ToString ();
-		gameOverMsg += " ";
+		if (score > 40.0)
+			msgToDisplay = winMsg;
+		else
+			msgToDisplay = gameOverMsg;
+			
+		msgToDisplay += score.ToString ();
+		msgToDisplay += " ";
 		yield return new WaitForSeconds (startDelay);
-		for (int i = 0; i < gameOverMsg.Length; i++) {
-			textComp.text = gameOverMsg.Substring (0,i);
+		for (int i = 0; i < msgToDisplay.Length; i++) {
+			textComp.text = msgToDisplay.Substring (0,i);
 			yield return new WaitForSeconds (typeDelay);
 		}
 	}
 
-	/*//auto delete text
-	public IEnumerator TypeOff(){
-		
-		for (int i = msg.Length; i >=0 ; i--) {
-			textComp.text = msg.Substring (0,i);
-			yield return new WaitForSeconds (typeDelay);
-		}
-	}*/
+
 
 
 }
